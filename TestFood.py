7e060f8e2c1290519ebf2e7fbad122ce7fddeed1
Albert Terc <albytterc@protonmail.com>
2022-07-04 14:06:34 -0400

    Fixed test style issue

--- TestFood.py
+++ TestFood.py
@@ -1,20 +1,20 @@
 import unittest
 from food import get_response, json_to_df, df_to_sql, print_info
 
+
 class TestFood(unittest.TestCase):
     def test_get_response(self):
-        
+        pass
+
     def test_json_to_df(self):
+        pass
 
     def test_df_to_sql(self):
+        pass
 
     def print_info(self):
         pass
 
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-    
